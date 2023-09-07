2349a113913843688ea5344607053dd00ad83ee1
alizashaikh6843 <144021821+alizashaikh6843@users.noreply.github.com>
2023-09-07 21:44:00 +0530

    Add files via upload

---
+++ sudoko.py
@@ -0,0 +1,55 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+def is_valid_move(board, row, col, num):
+    # Check row and column
+    if num in board[row] or num in [board[i][col] for i in range(9)]:
+        return False
+
+    # Check 3x3 box
+    box_row, box_col = row // 3 * 3, col // 3 * 3
+    for i in range(box_row, box_row + 3):
+        for j in range(box_col, box_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid_move(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0  # Backtrack if the solution is not found
+                return False
+    return True
+
+def main():
+    sudoku_board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Sudoku Puzzle:")
+    print_board(sudoku_board)
+
+    if solve_sudoku(sudoku_board):
+        print("\nSolution:")
+        print_board(sudoku_board)
+    else:
+        print("\nNo solution exists")
+
+if __name__ == "__main__":
+    main()
